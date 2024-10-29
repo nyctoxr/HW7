08d0788dc9cea336ce511834ad56a8e67771e1b0
Sajad <nyctoxr@gmail.com>
2024-10-29 17:05:59 +0330

    Moderator Model Class

---
+++ src/Model/Moderator.java
@@ -0,0 +1,27 @@
+package Model;
+
+public class Moderator {
+    private int id;
+    private String username;
+    private String password;
+
+    public Moderator(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}

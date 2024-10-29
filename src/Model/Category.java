c455b75dea1c1278d8bcf4271f1d43cc8d34ecc3
Sajad <nyctoxr@gmail.com>
2024-10-29 17:04:37 +0330

    Category Model Class

---
+++ src/Model/Category.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class Category {
+    private final int id;
+    private final String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return id + ": " + name;
+    }
+}
